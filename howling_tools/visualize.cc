7fc91b763f795113e1209891cdf7011cd8bcde2a
Natalie Wolfe <natalie@lifewanted.com>
2025-11-23 22:36:52 -0800

    Add analyzer support to visualizer.

--- howling_tools/visualize.cc
+++ howling_tools/visualize.cc
@@ -15,6 +15,9 @@
 #include "absl/time/time.h"
 #include "cli/colorize.h"
 #include "cli/printing.h"
+#include "data/analyzer.h"
+#include "data/analyzers/noop.h"
+#include "data/analyzers/zig_zag.h"
 #include "data/candle.pb.h"
 #include "data/stock.pb.h"
 #include "google/protobuf/text_format.h"
@@ -25,6 +28,7 @@
 
 ABSL_FLAG(std::string, stock, "", "Stock symbol to visualize.");
 ABSL_FLAG(std::string, date, "", "Day to visualize.");
+ABSL_FLAG(std::string, analyzer, "", "Name of an analyzer to run.");
 
 namespace howling {
 namespace {
@@ -64,6 +68,21 @@ stock::Symbol get_stock_symbol() {
   return symbol;
 }
 
+std::unique_ptr<analyzer> load_analyzer(const stock::History& history) {
+  std::string anal_name = absl::GetFlag(FLAGS_analyzer);
+  if (anal_name.empty() || anal_name == "noop") {
+    return std::make_unique<noop_analyzer>();
+  }
+  if (anal_name == "zig_zag" || anal_name == "optimal") {
+    return std::make_unique<zig_zag_analyzer>(
+        vector<Candle>{history.candles().begin(), history.candles().end()},
+        zig_zag_analyzer::options{.threshold = 0.5});
+  }
+  throw std::runtime_error(absl::StrCat("Unknown analyzer: ", anal_name));
+}
+
+std::string print_price(double price) { return std::format("{:.2f}", price); }
+
 void run() {
   stock::Symbol symbol = get_stock_symbol();
   std::string filename = absl::StrCat(absl::GetFlag(FLAGS_date), ".textproto");
@@ -76,6 +95,8 @@ void run() {
   }
 
   stock::History history = read_history(data_path);
+  auto anal = load_analyzer(history);
+
   print_extents extents{
       .min = std::numeric_limits<double>::max(),
       .max = std::numeric_limits<double>::min(),
@@ -89,6 +110,11 @@ void run() {
     max_close = std::max(max_close, candle.close());
   }
 
+  vector<Candle> observed;
+  int buy_counter = 0;
+  int sell_counter = 0;
+  double last_buy = std::numeric_limits<double>::max();
+  double profit = 0;
   for (const Candle& candle : history.candles()) {
     using namespace ::std::chrono;
     zoned_time opened_at{current_zone(), to_std_chrono(candle.opened_at())};
@@ -101,15 +127,38 @@ void run() {
       std::cout << "          | ";
     }
     std::cout << print_candle(candle, extents) << " | ";
-    if (candle.close() == min_close) {
-      std::cout << colorize(std::format("{:.2f}", candle.close()), color::RED);
+
+    observed.push_back(candle);
+    decision d = anal->analyze(aggregate(observed));
+    if (d.act == action::BUY) {
+      ++buy_counter;
+      last_buy = candle.low();
+      std::cout << colorize(print_price(candle.low()), color::RED) << " - Buy ("
+                << d.confidence << ")";
+    } else if (d.act == action::SELL) {
+      ++sell_counter;
+      profit += candle.high() - last_buy;
+      std::cout << colorize(print_price(candle.high()), color::GREEN)
+                << " - Sell (" << d.confidence << "; "
+                << print_price(candle.high() - last_buy) << ")";
+    } else if (candle.close() == min_close) {
+      std::cout << colorize(print_price(candle.close()), color::RED);
     } else if (candle.close() == max_close) {
-      std::cout << colorize(
-          std::format("{:.2f}", candle.close()), color::GREEN);
+      std::cout << colorize(print_price(candle.close()), color::GREEN);
     } else if (is_ref_point) {
-      std::cout << colorize(std::format("{:.2f}", candle.close()), color::GRAY);
+      std::cout << colorize(print_price(candle.close()), color::GRAY);
     }
     std::cout << '\n';
+  }
+  if (buy_counter > 0) {
+    std::cout << "\n"
+              << "Buys:   " << buy_counter << "\n"
+              << "Sells:  " << sell_counter << "\n"
+              << "Profit: "
+              << colorize(
+                     print_price(profit),
+                     profit > 0 ? color::GREEN : color::RED)
+              << "\n";
   }
 }
 
