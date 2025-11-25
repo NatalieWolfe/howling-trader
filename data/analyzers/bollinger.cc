071d33f43460f2630f54c3bfdced95c8a2ac2292
Natalie Wolfe <natalie@lifewanted.com>
2025-11-25 07:09:17 -0800

    Adjust bollinger band analizer

--- data/analyzers/bollinger.cc
+++ data/analyzers/bollinger.cc
@@ -11,17 +11,18 @@ namespace howling {
 decision
 bollinger_analyzer::analyze(stock::Symbol symbol, const trading_state& data) {
   const aggregations& market = data.market.at(symbol);
-  if (market.one_minute.size() < 2) {
+  if (market.one_minute.size() < 20) {
+    // Need a minimum of 20 minutes of data to assess the bands.
     return {.act = action::NO_ACTION, .confidence = 0.0};
   }
 
   if (market.one_minute(-1).candle.high() >
-          market.twenty_minute(-2).upper_bollinger_band &&
+          market.twenty_minute(-1).upper_bollinger_band &&
       can_sell(symbol, data)) {
     return {.act = action::SELL, .confidence = 1.0};
   }
   if (market.one_minute(-1).candle.low() <
-          market.twenty_minute(-2).lower_bollinger_band &&
+          market.twenty_minute(-1).lower_bollinger_band &&
       can_buy(symbol, data)) {
     return {.act = action::BUY, .confidence = 1.0};
   }
