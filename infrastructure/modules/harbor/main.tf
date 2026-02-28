abddbbe284b5c4f26dfd77731a5d1375ed28305d
Natalie Wolfe <natalie@lifewanted.com>
2026-02-27 21:55:39 -0800

    Add retention policy to prevent ballooning image registry

--- infrastructure/modules/harbor/main.tf
+++ infrastructure/modules/harbor/main.tf
@@ -3,3 +3,16 @@ resource "harbor_project" "main_project" {
   public                 = false
   vulnerability_scanning = true
 }
+
+resource "harbor_retention_policy" "main_retention" {
+  scope    = harbor_project.main_project.id
+  schedule = "Daily"
+
+  rule {
+    disabled             = false
+    most_recently_pushed = 5
+    repo_matching        = "{**}"
+    tag_matching         = "{*}"
+    untagged_artifacts   = true
+  }
+}
