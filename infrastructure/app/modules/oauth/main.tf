ee660cf627cd889f59da9fed96da088d4f1eb47f
Natalie Wolfe <natalie@lifewanted.com>
2026-03-16 20:44:16 -0700

    Switch sidecar to the agent proxy

--- infrastructure/app/modules/oauth/main.tf
+++ infrastructure/app/modules/oauth/main.tf
@@ -60,11 +60,11 @@ resource "kubernetes_deployment" "oauth" {
           app = "howling-oauth"
         }
         annotations = {
-          "vault.hashicorp.com/agent-inject"                    = "true"
-          "vault.hashicorp.com/role"                            = "howling-app-role"
-          "vault.hashicorp.com/agent-cache-enable"              = "true"
-          "vault.hashicorp.com/agent-cache-use-auto-auth-token" = "force"
-          "vault.hashicorp.com/agent-image"                     = var.openbao_agent_image
+          "openbao.org/agent-inject"                    = "true"
+          "openbao.org/role"                            = "howling-app-role"
+          "openbao.org/agent-proxy-enable"              = "true"
+          "openbao.org/agent-proxy-use-auto-auth-token" = "true"
+          "openbao.org/agent-image"                     = var.openbao_agent_image
         }
       }
 
