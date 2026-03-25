c38e7a7108a5276baedea88f13afa7c5b98ac10d
Natalie Wolfe <natalie@lifewanted.com>
2026-03-24 18:13:05 -0700

    Add utility to write contents to a file.
    Issue #119.

--- files/files.cc
+++ files/files.cc
@@ -1,14 +1,19 @@
 #include "files/files.h"
 
+#include <filesystem>
 #include <fstream>
 #include <iterator>
 #include <stdexcept>
+#include <string>
+#include <string_view>
 
 #include "absl/strings/str_cat.h"
 
 namespace howling::files {
 
-std::string read_file(const std::filesystem::path& path) {
+namespace fs = ::std::filesystem;
+
+std::string read_file(const fs::path& path) {
   std::ifstream file(path);
   if (!file.is_open()) {
     throw std::runtime_error(
@@ -17,6 +22,15 @@ std::string read_file(const std::filesystem::path& path) {
 
   return std::string(
       (std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+}
+
+void write_file(const fs::path& path, std::string_view content) {
+  std::ofstream file(path, std::ios::trunc);
+  if (!file.is_open()) {
+    throw std::runtime_error(
+        absl::StrCat("Failed to open file for write: ", path.string()));
+  }
+  file << content << std::flush;
 }
 
 } // namespace howling::files
