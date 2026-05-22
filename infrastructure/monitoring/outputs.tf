9aeef6aa0bd9e3d46110cdb7c811a9bf5109ce9e
Natalie Wolfe <natalie@lifewanted.com>
2026-05-21 19:44:18 -0700

    fmt

--- infrastructure/monitoring/outputs.tf
+++ infrastructure/monitoring/outputs.tf
@@ -1,9 +1,9 @@
 output "grafana_ingress_ip" {
   value = try(
     kubernetes_ingress_v1.grafana.status[0]
-      .load_balancer[0].ingress[0].ip,
+    .load_balancer[0].ingress[0].ip,
     kubernetes_ingress_v1.grafana.status[0]
-      .load_balancer[0].ingress[0].hostname,
+    .load_balancer[0].ingress[0].hostname,
     "pending"
   )
   description = "The public IP address or hostname of the Grafana ingress"
